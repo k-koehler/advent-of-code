c9e8ee31a8b98564128c6f63d314afe814628881
kevin-koehler <kevin.koehler@usask.ca>
2019-12-02 10:23:56 -0600

    part 2

--- day1/main.c
+++ day1/main.c
@@ -1,16 +1,19 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-const char **FILENAME = "input.in";
-const unsigned int INPUT_SIZE = 100;
+typedef int64_t i64;
 
-unsigned int *read_file() {
+const char *FILENAME = "input.in";
+const i64 INPUT_SIZE = 100;
+
+i64 *read_file() {
   FILE *fp;
   if ((fp = fopen(FILENAME, "r")) == NULL) {
     printf("err opening file", stderr);
     exit(EXIT_FAILURE);
   }
-  unsigned int module_masses[INPUT_SIZE];
+  i64 *module_masses = (i64 *)malloc(sizeof(i64) * INPUT_SIZE);
   char line[256];
   size_t counter = 0;
   while (fgets(line, sizeof(line), fp)) {
@@ -20,14 +23,35 @@ unsigned int *read_file() {
   return module_masses;
 }
 
-unsigned int calculate_mass(unsigned int mass) { return (mass / 3) - 2; }
+i64 sgn(i64 x) { return x >= 0 ? x : 0u; }
+
+i64 calculate_mass(i64 mass) { return (mass / 3) - 2; }
+
+i64 part1(i64 *masses) {
+  i64 total_fuel = 0;
+  for (size_t i = 0; i < INPUT_SIZE; ++i) {
+    total_fuel += sgn(calculate_mass(masses[i]));
+  }
+  return total_fuel;
+}
+
+i64 part2(i64 *masses) {
+  i64 total_fuel = 0;
+  for (size_t i = 0; i < INPUT_SIZE; ++i) {
+    i64 fuel = sgn(calculate_mass(masses[i]));
+    while (fuel != 0) {
+      total_fuel += fuel;
+      fuel = sgn(calculate_mass(fuel));
+    }
+  }
+  return total_fuel;
+}
 
 int main() {
-  int *masses = read_file();
-  unsigned int total_fuel = 0;
-  for (size_t i = 0; i < INPUT_SIZE; ++i) {
-    total_fuel += calculate_mass(masses[i]);
-  }
-  printf("The total fuel requirement is: %d!", total_fuel);
+  i64 *masses = read_file();
+  printf("The total fuel requirement for part 1 is: %d!", part1(masses));
+  putc('\n', stdout);
+  printf("The total fuel requirement for part 2 is: %d!", part2(masses));
+  free(masses);
   return EXIT_SUCCESS;
 }
